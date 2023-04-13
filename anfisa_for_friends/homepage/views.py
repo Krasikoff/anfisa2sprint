c7e50fe8c8d0e2414aa480298179356fee381a48
Красиков Дмитрий <z1r0-d@yandex.ru>
2023-04-13 15:54:03 +0300

    Before complex query

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -2,18 +2,19 @@ from django.db.models import Q
 from django.shortcuts import render
 
 
-from ice_cream.models import IceCream
+from ice_cream.models import Category, IceCream
 
 
 def index(request):
     template_name = 'homepage/index.html'
-    # Возьмём нужное. А ненужное не возьмём:
+    
     ice_cream_list = IceCream.objects.values(
         'id','title','description'
     ).filter(
         (Q(is_on_main=True) & Q(is_published=True))
       | (Q(title__contains='пломбир') & Q(is_published=True))
-    ) 
+    )
+ 
     context = {
         'ice_cream_list': ice_cream_list,
     }
